4d4a0645f8913ddb9780184968fe08b83dbc056b
sowden <sowden@1cbb81ce-7c37-0410-a547-63cdf85b527f>
2007-08-18 20:57:29 +0000

    file structure compleatly changed, added lots of stuff

---
+++ trunk/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+// public path to this file
+$self = "/todo/index.php";
+// relative path to your taskpaper document
+$file = 'todo-txt.txt';
+// Display instructions
+$instructions = true;
+
+?>
