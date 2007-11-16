a61e0ec71e9135b61cdfb00652cf5dc14689cb98
sowden <sowden@1cbb81ce-7c37-0410-a547-63cdf85b527f>
2007-11-16 10:23:45 +0000

    change to make errors work under different servers

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 
-// public path to the ajax file
-$self = "/todo/ajax.php";
+// public path to the ajax file (with leading slash)
+$self = "./ajax.php";
 // relative path to your taskpaper document
 $file = 'todo-txt.txt';
 // do you want the file to be made writable if it isn't onupload
